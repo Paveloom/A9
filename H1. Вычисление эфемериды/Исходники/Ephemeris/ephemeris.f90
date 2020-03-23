b2e61a355db8081e168da671567534eb377b35e7
Pavel Sobolev <Paveloom@mail.ru>
2020-03-23 17:53:14 +0000

    Описан корпус.

---
+++ H1. Вычисление эфемериды/Исходники/Ephemeris/ephemeris.f90
@@ -0,0 +1,16 @@
+module ephemeris ! Модуль, определяющий интерфейс для
+                 ! вычисления эфемерид малых планет
+use prec_m
+implicit none
+     
+     private
+     public :: ephemeris_API ! Тип, определяющий API модуля
+     
+     ! Тип, определяющий API модуля
+     type ephemeris_API
+          
+          
+          
+     end type ephemeris_API
+     
+end module ephemeris
